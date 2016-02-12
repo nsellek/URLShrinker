ad7a592ddb26c2abdd7a7ad378a0d5148acdfbb7
Nicholas Sellek <ngsellek@gmail.com>
2016-02-12 10:16:41 -0500

    Changed README

--- app/models/url_shortener.rb
+++ app/models/url_shortener.rb
@@ -1,8 +1,8 @@
 class UrlShortener < ActiveRecord::Base
   validates :URL, presence: true
-  @o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
 
   def self.shorturl
+    o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
     (0...8).map { @o[rand(@o.length)] }.join
   end
 end
